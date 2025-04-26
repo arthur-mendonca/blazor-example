a78272054dfa00a4e536373f98d21e6a2048d084
arthur-mendonca <arthu91@gmail.com>
2025-04-26 19:08:23 -0300

    criar model de Produto

---
+++ BlazorApp1/Models/Products.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Imagem { get; set; }
+    }
+}
